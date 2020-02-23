37181f36991f4d884b8a5cd571816c9f4e315a82
Julio Saraiva <contato@juliosaraiva.com.br>
2020-02-23 08:00:29 -0300

    feat: user authentication

---
+++ src/views/LoginUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <label for="email">Email:</label>
+      <input type="email" v-model="email" name="email" />
+
+      <label for="password">Password:</label>
+      <input type="password" v-model="password" name="password" />
+
+      <button type="submit" name="button">Login</button>
+      <router-link to="/register">Already have an account? Register.</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginUser',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      this.$store
+        .dispatch('login', {
+          email: this.email,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' })
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form @submit.prevent="register">
+      <label for="name">Name:</label>
+      <input v-model="name" name="name" type="text" />
+      <label for="email">Email:</label>
+      <input v-model="email" name="email" type="email" />
+      <label for="password">Password:</label>
+      <input v-model="password" name="password" type="password" />
+      <button type="submit" name="button">Register</button>
+      <router-link to="/login">Already have an account? Login.</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register() {
+      this.$store
+        .dispatch('register', {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' })
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
